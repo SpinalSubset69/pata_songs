ef3d54e1b69d496218c4031093f6cad767c71418
Alan Franco <alanfrancob97@gmail.com>
2025-07-09 22:49:13 -0600

    feat: add pata_logger module for improved logging

---
+++ src/pata_logger.py
@@ -0,0 +1,25 @@
+import logging
+from typing import TextIO
+
+def Logger(name: str) -> logging.Logger:
+    logging.basicConfig(level=logging.DEBUG)
+
+    console_handler: logging.StreamHandler[TextIO] = logging.StreamHandler();
+    file_handler: logging.FileHandler = logging.FileHandler(
+        filename="./logs/logs.txt",
+        encoding="utf-8",
+        mode='w'
+    )
+
+    formatter: logging.Formatter = logging.Formatter(
+        "%(asctime)s %(levelname)-8s %(name)s.%(funcName)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger: logging.Logger = logging.getLogger(name)
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
